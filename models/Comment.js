cb3e6eb28329d55173f680aa97e490526ea6f391
Brenda Garcia <brenda.garcia007@mymdc.net>
2019-04-19 19:58:49 -0400

    added models

---
+++ models/Comment.js
@@ -0,0 +1,11 @@
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+
+//create CommentSchema object
+var CommentSchema = new Schema({
+    title: String,
+    body: String
+})
+
+var Comment = mongoose.model("Comment", CommentSchema);
+module.exports = Comment;
